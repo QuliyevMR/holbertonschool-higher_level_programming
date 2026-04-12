9bedb939dfbf23a23a17184aa23d8f5036bae074
QuliyevMR <mrquliyev2007@gmail.com>
2026-04-12 23:12:58 +0400

    Update 100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    if len(sys.stdin.argv) != 4:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
@@ -24,4 +24,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     print("{} {} {} = {}".format(a, operator, b, result))
-  
+    

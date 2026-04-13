8852aed18cdc661e9f1be2a1fba3e2a0312551ce
QuliyevMR <mrquliyev2007@gmail.com>
2026-04-13 21:47:03 +0400

    Create 5-number_keys.py

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    Lüğətdəki açarların sayını qaytaran funksiya.
+    
+    Args:
+        a_dictionary: Giriş lüğəti.
+        
+    Returns:
+        Açarların sayı (integer).
+    """
+    
+    # len() funksiyası lüğətin birbaşa açarlarının sayını hesablayır.
+    return len(a_dictionary)

45a0c8d34940fec164153576f903d5131dd13d8d
QuliyevMR <mrquliyev2007@gmail.com>
2026-04-13 21:54:05 +0400

    Update 5-number_keys.py

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-
 def number_keys(a_dictionary):
     """
-    Lüğətdəki açarların sayını qaytaran funksiya.
-    
-    Args:
-        a_dictionary: Giriş lüğəti.
-        
-    Returns:
-        Açarların sayı (integer).
+    Returns the number of keys in a dictionary.
     """
-    
-    # len() funksiyası lüğətin birbaşa açarlarının sayını hesablayır.
     return len(a_dictionary)

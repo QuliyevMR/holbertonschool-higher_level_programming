6b3a4a258f1a0ff0f674a37168526b9850d2dedd
QuliyevMR <mrquliyev2007@gmail.com>
2026-04-18 21:08:23 +0400

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 """
-This module provides a function that adds two integers.
+This module provides a function
+that adds two integers.
+It handles casting and type checking.
 """
 
 
 def add_integer(a, b=98):
     """
     Adds two integers or floats after casting to int.
-    If a or b are NaN, raises a TypeError.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    # Yalnız NaN yoxlanışı qalır.
-    # Sonsuzluq (inf) isə int() funksiyasına gedib təbii olaraq OverflowError verəcək.
     if a != a:
         raise TypeError("a must be an integer")
     if b != b:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
-    

77cbbcbced1048e12129e612d96b3b98e302debd
QuliyevMR <mrquliyev2007@gmail.com>
2026-04-18 21:19:03 +0400

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,9 +10,18 @@ def add_integer(a, b=98):
     """
     Adds two integers or floats after casting to int.
     """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
+
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    # NaN check
+    if isinstance(a, float) and a != a:
+        raise ValueError("cannot convert float NaN to integer")
+
+    if isinstance(b, float) and b != b:
+        raise ValueError("cannot convert float NaN to integer")
 
     return int(a) + int(b)

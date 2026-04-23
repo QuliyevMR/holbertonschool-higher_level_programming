cec2e926b200b1d6bd511a46b45d22c96c68822f
QuliyevMR <mrquliyev2007@gmail.com>
2026-04-23 18:55:08 +0400

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""Module that defines a function to indent text."""
+"""Module that defines text indentation."""
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines after '.', '?' and ':' characters."""
+    """Prints text with 2 new lines after '.', '?' and ':'."""
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    i = 0
-    n = len(text)
+    skip_space = False
 
-    while i < n:
-        print(text[i], end="")
-
-        if text[i] in ".?:":
-            print("\n")
-
-            # skip spaces after punctuation
-            i += 1
-            while i < n and text[i] == " ":
-                i += 1
+    for char in text:
+        if skip_space and char == " ":
             continue
 
-        i += 1
+        skip_space = False
+
+        print(char, end="")
+
+        if char in ".?:":
+            print("\n")
+            skip_space = True

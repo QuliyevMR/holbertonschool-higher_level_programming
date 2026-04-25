6108e082b0c1f86c4359fe2c5354afdf61ca109a
QuliyevMR <mrquliyev2007@gmail.com>
2026-04-25 19:45:49 +0400

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,16 +2,14 @@
 """
 Bu modul Kvadrat klassını təyin edir.
 """
-
 class Square:
     """
     Kvadratı təmsil edən klass.
     """
-
     def __init__(self, size):
         """
         Kvadrat yaradılan zaman işə düşən funksiya (constructor).
         Args:
             size: Kvadratın tərəfinin ölçüsü.
         """
-        self.__size = size  # Budur əsas məqam!
+        self.__size = size

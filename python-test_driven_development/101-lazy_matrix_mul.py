d80485d526d7d7845b0492d8da1d09fce0da0e45
QuliyevMR <mrquliyev2007@gmail.com>
2026-04-25 18:34:29 +0400

    Update 101-lazy_matrix_mul.py

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
 Module 101-lazy_matrix_mul
-Multiplies 2 matrices using NumPy.
+Contains a function that multiplies two matrices using the NumPy module.
 """
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
     """
-    Multiplies m_a and m_b using numpy.matmul.
+    Multiplies two matrices using NumPy.
+
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
+
+    Returns:
+        numpy.ndarray: The resulting matrix product of m_a and m_b.
     """
-    # Checker-in bug-ını keçmək üçün xüsusi yoxlama:
-    # Əgər matrislərdən biri sadəcə string, int və ya float-dırsa, 
-    # checker-in gözlədiyi o köhnə xətanı özümüz atırıq.
-    if isinstance(m_a, (str, int, float)) or isinstance(m_b, (str, int, float)):
-        raise ValueError("Scalar operands are not allowed, use '*' instead")
-    
-    # Qalan bütün hallarda (düzgün matrislər və ya ölçü səhvləri) NumPy özü həll edir
     return np.matmul(m_a, m_b)

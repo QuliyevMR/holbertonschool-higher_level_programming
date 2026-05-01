049f228663c346c918436203d676a18262c77701
QuliyevMR <mrquliyev2007@gmail.com>
2026-05-01 18:20:52 +0400

    Create 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Bu modul BaseGeometry sinfini ehtiva edir.
+"""
+
+
+class BaseGeometry:
+    """Həndəsi fiqurlar üçün əsas sinif."""
+
+    def area(self):
+        """
+        Sahəni hesablayır.
+        
+        Raises:
+            Exception: Əgər alt sinif bu metodu implement etməyibsə.
+        """
+        raise Exception("area() is not implemented")

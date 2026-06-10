377d823d6f31318b7d4426c364420d0ecf119fbe
QuliyevMR <mrquliyev2007@gmail.com>
2026-06-11 03:58:58 +0400

    Update 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines a text file-writing function."""
 
-
 def write_file(filename="", text=""):
     """Writes a string to a text file (UTF8) and returns the number of characters written."""
     with open(filename, "w", encoding="utf-8") as f:

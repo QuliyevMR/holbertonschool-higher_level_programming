c9c748a7c961acc842d1b22cff4ba70e2eba472d
QuliyevMR <mrquliyev2007@gmail.com>
2026-06-15 05:18:01 +0400

    Update 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -23,9 +23,9 @@ class Student:
         Student instansiyasının lüğət təsvirini qaytarır.
         Əgər attrs siyahısı verilibsə, yalnız həmin atributları filtrləyir.
         """
-        # attrs-ın siyahı olub-olmadığını və bütün elementlərinin string olduğunu yoxlayırıq
-        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+        # Sətiri 79 simvoldan qısa saxlamaq üçün yoxlamanı bölürük
+        if (isinstance(attrs, list) and
+                all(isinstance(x, str) for x in attrs)):
             return {k: v for k, v in self.__dict__.items() if k in attrs}
 
-        # Əks halda bütün atributları qaytarırıq
         return self.__dict__

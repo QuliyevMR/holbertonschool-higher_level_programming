e5cd1b86a01e6a8a74d1c9270f04838090942bcd
QuliyevMR <mrquliyev2007@gmail.com>
2026-06-15 06:38:09 +0400

    Create 7-insert_value.sql

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- 'first_table' cədvəlinə yeni bir sətir əlavə edir.
+-- id sütununa 89, name sütununa 'Best School' dəyərlərini daxil edir.
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
